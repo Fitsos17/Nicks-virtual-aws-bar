0e2b05746bae96326a56176986a4ebb3ef781060
Nick <Nikolas.amiridis@gmail.com>
2025-07-31 18:53:27 +0300

    Initial commit

---
+++ test/self-service-ordering.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const SelfServiceOrdering = require('../lib/self-service-ordering-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/self-service-ordering-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new SelfServiceOrdering.SelfServiceOrderingStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
