784bf85156120fced44ae0de27d93bec855d3f8d
Nick <Nikolas.amiridis@gmail.com>
2025-08-01 22:55:26 +0300

    Developed the welcome page and created json file with the first drinks of the bar

--- functions/welcome.js
+++ functions/welcome.js
@@ -1,8 +1,17 @@
 exports.handler = async (event) => {
   return {
     statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify({
-      path: event.path,
+      welcome: "Helo everybody and welcome to nicks' bar! 🍻",
+      tablesAndSunbeds:
+        "Find all of the free tables and sunbeds in the /tables-sunbeds route. Make sure to grab a sit and enjoy a refreshing drink! 🪑",
+      calatalog:
+        "You can find the catalog with all of the drinks in the /catalog route. Find which drink you want the most and order it! 🍹",
+      order:
+        "When you are ready to order, go to the /order route and enter the id of the drink you want to order. Our experienced barmen will make them as quickly and as soon as possible, so you'll have a great experience! 🍾",
     }),
   };
 };
