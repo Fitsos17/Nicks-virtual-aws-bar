cd8b37f37259b3333c66da9c515120201ca66778
Fitsos17 <nikolas.amiridis@gmail.com>
2025-09-03 12:06:08 +0300

    Corrected the route in the welcome function (order -> orders)

--- functions/welcome.js
+++ functions/welcome.js
@@ -11,8 +11,8 @@ exports.handler = async (event) => {
       "Find all of the free tables and sunbeds in the /seats route. Grab a seat and enjoy a refreshing drink! 🪑",
     calatalog:
       "You can find the catalog with all of the drinks in the /catalog route. Find which drink you want the most and order it! 🍹",
-    order:
-      "When you are ready to order, go to the /order route and enter the id of the drink you want to order. Our experienced barmen will make them as quickly and as soon as possible, so you'll have a great experience! 🍾",
+    orders:
+      "When you are ready to order, go to the /orders route and enter the id of the drink you want to order. Our experienced barmen will make them as quickly and as soon as possible, so you'll have a great experience! 🍾",
   };
 
   return handleReturningOfRouteFunctions(body);
